a13066737322759ab150e3023365ecbb60ac1cb6
anttorre <antonio.torre@everis.com>
2022-09-15 13:35:41 +0200

    Enabling onboarding processor binding when context ready

--- src/main/java/it/gov/pagopa/admissibility/event/processor/AdmissibilityProcessorConfig.java
+++ src/main/java/it/gov/pagopa/admissibility/event/processor/AdmissibilityProcessorConfig.java
@@ -3,9 +3,15 @@ package it.gov.pagopa.admissibility.event.processor;
 
 import it.gov.pagopa.admissibility.dto.onboarding.EvaluationDTO;
 import it.gov.pagopa.admissibility.service.AdmissibilityEvaluatorMediatorService;
+import it.gov.pagopa.admissibility.service.onboarding.OnboardingContextHolderServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.binder.Binding;
+import org.springframework.cloud.stream.binder.BindingCreatedEvent;
+import org.springframework.cloud.stream.binding.BindingsLifecycleController;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.Message;
 import reactor.core.publisher.Flux;
 
@@ -13,20 +19,42 @@ import java.util.function.Function;
 
 @Configuration
 @Slf4j
-public class AdmissibilityProcessorConfig {
+public class AdmissibilityProcessorConfig implements ApplicationListener<OnboardingContextHolderServiceImpl.OnboardingContextHolderReadyEvent> {
 
+    public static final String ADMISSIBILITY_PROCESSOR_BINDING_NAME = "admissibilityProcessor-in-0";
+    private final BindingsLifecycleController bindingsLifecycleController;
     private final AdmissibilityEvaluatorMediatorService admissibilityEvaluatorMediatorService;
 
-    public AdmissibilityProcessorConfig(AdmissibilityEvaluatorMediatorService admissibilityEvaluatorMediatorService) {
+    private boolean contextReady = false;
+
+    public AdmissibilityProcessorConfig(BindingsLifecycleController bindingsLifecycleController, AdmissibilityEvaluatorMediatorService admissibilityEvaluatorMediatorService) {
+        this.bindingsLifecycleController = bindingsLifecycleController;
         this.admissibilityEvaluatorMediatorService = admissibilityEvaluatorMediatorService;
     }
 
     /**
-     *  Read from the topic ${KAFKA_TOPIC_ONBOARDING} and publish to topic ${KAFKA_TOPIC_ONBOARDING_OUTCOME}
+     * Read from the topic ${KAFKA_TOPIC_ONBOARDING} and publish to topic ${KAFKA_TOPIC_ONBOARDING_OUTCOME}
      */
     @Bean
     public Function<Flux<Message<String>>, Flux<EvaluationDTO>> admissibilityProcessor() {
         return this.admissibilityEvaluatorMediatorService::execute;
+    }
+
+    @EventListener(BindingCreatedEvent.class)
+    public void onBindingCreatedEvent(BindingCreatedEvent event) {
+        if (event.getSource() instanceof Binding<?> binding && ADMISSIBILITY_PROCESSOR_BINDING_NAME.equals(binding.getBindingName()) && !contextReady) {
+            synchronized (this) {
+                binding.stop();
+            }
+        }
+    }
+
+    @Override
+    public void onApplicationEvent(OnboardingContextHolderServiceImpl.OnboardingContextHolderReadyEvent event) {
+        synchronized (this) {
+            contextReady = true;
+            bindingsLifecycleController.changeState(ADMISSIBILITY_PROCESSOR_BINDING_NAME, BindingsLifecycleController.State.STARTED);
+        }
     }
 
 }
--- src/main/java/it/gov/pagopa/admissibility/service/onboarding/OnboardingContextHolderServiceImpl.java
+++ src/main/java/it/gov/pagopa/admissibility/service/onboarding/OnboardingContextHolderServiceImpl.java
@@ -6,27 +6,36 @@ import it.gov.pagopa.admissibility.repository.DroolsRuleRepository;
 import it.gov.pagopa.admissibility.service.build.KieContainerBuilderService;
 import lombok.extern.slf4j.Slf4j;
 import org.kie.api.runtime.KieContainer;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 @Service
 @Slf4j
 public class OnboardingContextHolderServiceImpl implements OnboardingContextHolderService {
-    private final KieContainerBuilderService kieContainerBuilderService;
 
+    private final KieContainerBuilderService kieContainerBuilderService;
     private final DroolsRuleRepository droolsRuleRepository;
 
     private KieContainer kieContainer;
     private final Map<String, InitiativeConfig> initiativeId2Config=new HashMap<>();
 
-    public OnboardingContextHolderServiceImpl(KieContainerBuilderService kieContainerBuilderService, DroolsRuleRepository droolsRuleRepository) {
+    public OnboardingContextHolderServiceImpl(KieContainerBuilderService kieContainerBuilderService, DroolsRuleRepository droolsRuleRepository, ApplicationEventPublisher applicationEventPublisher) {
         this.kieContainerBuilderService = kieContainerBuilderService;
         this.droolsRuleRepository = droolsRuleRepository;
-        refreshKieContainer();
+        refreshKieContainer(x -> applicationEventPublisher.publishEvent(new OnboardingContextHolderReadyEvent(this)));
+    }
+
+    public static class OnboardingContextHolderReadyEvent extends ApplicationEvent {
+        public OnboardingContextHolderReadyEvent(Object source) {
+            super(source);
+        }
     }
 
     //region kieContainer holder
@@ -44,10 +53,15 @@ public class OnboardingContextHolderServiceImpl implements OnboardingContextHold
     // TODO use cache
     @Scheduled(fixedRateString = "${app.beneficiary-rule.cache.refresh-ms-rate}")
     public void refreshKieContainer(){
+        refreshKieContainer(x -> log.trace("Refreshed KieContainer"));
+    }
+
+    //TODO use cache
+    public void refreshKieContainer(Consumer<? super KieContainer> subscriber){
         log.trace("[BENEFICIARY_RULE_BUILDER] Refreshing KieContainer");
         final Flux<DroolsRule> droolsRuleFlux = droolsRuleRepository.findAll().doOnNext(dr -> setInitiativeConfig(dr.getInitiativeConfig()));
 
-        kieContainerBuilderService.build(droolsRuleFlux).subscribe(this::setBeneficiaryRulesKieContainer);
+        kieContainerBuilderService.build(droolsRuleFlux).doOnNext(this::setBeneficiaryRulesKieContainer).subscribe(subscriber);
     }
     //endregion
 
