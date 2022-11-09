a20008916da245db9c9b77bd6404448180794f8f
pelliccm <matteo.pelliccione.sa@nttdata.com>
2022-11-09 12:53:46 +0100

    ErrorDTO update

--- src/test/java/it/gov/pagopa/admissibility/exception/ErrorManagerTest.java
+++ src/test/java/it/gov/pagopa/admissibility/exception/ErrorManagerTest.java
@@ -35,7 +35,7 @@ class ErrorManagerTest extends BaseIntegrationTest {
     void handleExceptionClientExceptionWithBody(){
         Mockito.when(admissibilityController.getInitiativeStatus("ClientExceptionWithBody"))
                 .thenThrow(new ClientExceptionWithBody(HttpStatus.BAD_REQUEST, "Error","Error ClientExceptionWithBody"));
-        ErrorDTO errorClientExceptionWithBody= new ErrorDTO(Severity.ERROR,"Error","Error ClientExceptionWithBody");
+        ErrorDTO errorClientExceptionWithBody= new ErrorDTO("Error","Error ClientExceptionWithBody");
 
         webTestClient.get()
                 .uri(uriBuilder -> uriBuilder.path("/idpay/admissibility/initiative/{initiativeId}")
@@ -46,7 +46,7 @@ class ErrorManagerTest extends BaseIntegrationTest {
 
         Mockito.when(admissibilityController.getInitiativeStatus("ClientExceptionWithBodyWithStatusAndTitleAndMessageAndThrowable"))
                 .thenThrow(new ClientExceptionWithBody(HttpStatus.BAD_REQUEST, "Error","Error ClientExceptionWithBody", new Throwable()));
-        ErrorDTO errorClientExceptionWithBodyWithStatusAndTitleAndMessageAndThrowable = new ErrorDTO(Severity.ERROR,"Error","Error ClientExceptionWithBody");
+        ErrorDTO errorClientExceptionWithBodyWithStatusAndTitleAndMessageAndThrowable = new ErrorDTO("Error","Error ClientExceptionWithBody");
 
         webTestClient.get()
                 .uri(uriBuilder -> uriBuilder.path("/idpay/admissibility/initiative/{initiativeId}")
@@ -58,7 +58,7 @@ class ErrorManagerTest extends BaseIntegrationTest {
 
     @Test
     void handleExceptionClientExceptionTest(){
-        ErrorDTO expectedErrorClientException = new ErrorDTO(Severity.ERROR,"Error","Something gone wrong");
+        ErrorDTO expectedErrorClientException = new ErrorDTO("Error","Something gone wrong");
 
         Mockito.when(admissibilityController.getInitiativeStatus("ClientException"))
                 .thenThrow(ClientException.class);
@@ -91,7 +91,7 @@ class ErrorManagerTest extends BaseIntegrationTest {
 
     @Test
     void handleExceptionRuntimeException(){
-        ErrorDTO expectedErrorDefault = new ErrorDTO(Severity.ERROR,"Error","Something gone wrong");
+        ErrorDTO expectedErrorDefault = new ErrorDTO("Error","Something gone wrong");
 
         Mockito.when(admissibilityController.getInitiativeStatus("RuntimeException"))
                 .thenThrow(RuntimeException.class);
