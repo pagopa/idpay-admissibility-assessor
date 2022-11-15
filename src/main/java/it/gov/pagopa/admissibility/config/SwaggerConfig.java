e6b3fed90d1cbe3eac439e740bd8cd1b7edfc6a7
anttorre <antonio.torre@nttdata.com>
2022-11-15 15:01:36 +0100

    fix Null not more returned and page model fix in swagger

--- src/main/java/it/gov/pagopa/admissibility/config/SwaggerConfig.java
+++ src/main/java/it/gov/pagopa/admissibility/config/SwaggerConfig.java
@@ -2,21 +2,30 @@ package it.gov.pagopa.admissibility.config;
 
 import com.fasterxml.classmate.TypeResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.AlternateTypeBuilder;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.CachingModelProvider;
+import springfox.documentation.schema.ModelProvider;
+import springfox.documentation.schema.ModelSpecification;
+import springfox.documentation.schema.ModelSpecificationProvider;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.schema.contexts.ModelContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.lang.reflect.Type;
 import java.time.LocalTime;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 import static springfox.documentation.schema.AlternateTypeRules.newRule;
@@ -46,7 +55,6 @@ public class SwaggerConfig {
 	 * @param typeResolver the type resolver
 	 * @return the docket
 	 */
-
 	@Bean
 	public Docket swaggerSpringPlugin(@Autowired TypeResolver typeResolver) {
 		return (new Docket(DocumentationType.OAS_30)).select().apis(RequestHandlerSelectors.any())
@@ -76,4 +84,29 @@ public class SwaggerConfig {
 				.property(b->b.name("sort").type(String.class).canRead(true).canWrite(true))
 				.build();
 	}
+
+	@Bean
+	@Primary
+	@Qualifier("cachedModels")
+	public ModelSpecificationProvider customModelSpecificationProvider(
+			@SuppressWarnings({"squid:S1874", "deprecation"}) // suppressed, caused used by the library service
+			@Qualifier("default") ModelProvider delegate,
+			@Qualifier("default") ModelSpecificationProvider specificationDelegate){
+		return new CachingModelProvider(delegate, specificationDelegate){
+			@Override
+			public Optional<ModelSpecification> modelSpecificationsFor(ModelContext modelContext) {
+				Optional<ModelSpecification> spec = super.modelSpecificationsFor(modelContext);
+				if(Page.class.equals(modelContext.getType().getErasedType())){
+					spec.flatMap(ModelSpecification::getCompound)
+							.ifPresent(m->m
+									.getProperties()
+									.removeIf(f->"pageable".equals(f.getName())));
+					return spec;
+				} else {
+					return spec;
+				}
+			}
+		};
+	}
+
 }
