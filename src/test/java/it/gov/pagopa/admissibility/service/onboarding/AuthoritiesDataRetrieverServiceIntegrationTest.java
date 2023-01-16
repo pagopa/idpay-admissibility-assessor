900eb050a4feab1a18c8a71fc8640b5f5db58520
pelliccm <matteo.pelliccione.sa@nttdata.com>
2023-01-16 11:47:13 +0100

    IDP-94 integration test

--- src/test/java/it/gov/pagopa/admissibility/service/onboarding/AuthoritiesDataRetrieverServiceIntegrationTest.java
+++ src/test/java/it/gov/pagopa/admissibility/service/onboarding/AuthoritiesDataRetrieverServiceIntegrationTest.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.admissibility.service.onboarding;
 
 import it.gov.pagopa.admissibility.BaseIntegrationTest;
 import it.gov.pagopa.admissibility.dto.onboarding.OnboardingDTO;
+import it.gov.pagopa.admissibility.dto.onboarding.extra.BirthDate;
+import it.gov.pagopa.admissibility.dto.onboarding.extra.Residence;
 import it.gov.pagopa.admissibility.mapper.TipoResidenzaDTO2ResidenceMapper;
 import it.gov.pagopa.admissibility.model.InitiativeConfig;
 import it.gov.pagopa.admissibility.service.pdnd.CreateTokenService;
@@ -10,14 +12,18 @@ import it.gov.pagopa.admissibility.service.pdnd.residence.ResidenceAssessmentSer
 import it.gov.pagopa.admissibility.utils.RestTestUtils;
 import it.gov.pagopa.admissibility.utils.TestUtils;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.List;
 
 class AuthoritiesDataRetrieverServiceIntegrationTest extends BaseIntegrationTest {
 
@@ -33,10 +39,38 @@ class AuthoritiesDataRetrieverServiceIntegrationTest extends BaseIntegrationTest
     @Autowired
     private AuthoritiesDataRetrieverService authoritiesDataRetrieverService;
 
+    private OnboardingDTO onboardingDTO;
+    private InitiativeConfig initiativeConfig;
+    private Message<String> message;
 
     @BeforeEach
     void setUp() {
+        onboardingDTO = OnboardingDTO.builder()
+                .userId("USERID_OK_1")
+                .initiativeId("INITIATIVEID")
+                .tc(true)
+                .status("STATUS")
+                .pdndAccept(true)
+                .tcAcceptTimestamp(LocalDateTime.of(2022,10,2,10,0,0))
+                .criteriaConsensusTimestamp(LocalDateTime.of(2022,10,2,10,0,0))
+                .build();
 
+        LocalDate now = LocalDate.now();
+        initiativeConfig = InitiativeConfig.builder()
+                .initiativeId("INITIATIVEID")
+                .initiativeName("INITITIATIVE_NAME")
+                .organizationId("ORGANIZATIONID")
+                .status("STATUS")
+                .startDate(now)
+                .endDate(now)
+                .pdndToken("PDND_TOKEN")
+                .initiativeBudget(new BigDecimal("100"))
+                .beneficiaryInitiativeBudget(BigDecimal.TEN)
+                .rankingInitiative(Boolean.TRUE)
+                .automatedCriteriaCodes(List.of("ISEE", "RESIDENCE", "BIRTHDATE"))
+                .build();
+
+        message = MessageBuilder.withPayload(TestUtils.jsonSerializer(onboardingDTO)).build();
 
         RestTestUtils.USE_TRUSTSTORE_KO = true;
         BaseIntegrationTest.initServerWiremock();
@@ -51,6 +85,21 @@ class AuthoritiesDataRetrieverServiceIntegrationTest extends BaseIntegrationTest
     @Test
     void test() {
 
-       // OnboardingDTO result = authoritiesDataRetrieverService.retrieve(onboardingDTO, initiativeConfig, );
+        OnboardingDTO result = authoritiesDataRetrieverService.retrieve(onboardingDTO, initiativeConfig, message).block();
+
+        Residence expectedResidence = Residence.builder()
+                .postalCode("20143")
+                .province("MI")
+                .city("Milano")
+                .build();
+        BirthDate expectedBirthDate = BirthDate.builder()
+                .year("1970")
+                .age(Period.between(LocalDate.of(1970,1,1), LocalDate.now()).getYears())  // 2023-1970=53
+                .build();
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(BigDecimal.valueOf(24428), result.getIsee());   // TODO INPS
+        Assertions.assertEquals(expectedResidence, result.getResidence());
+        Assertions.assertEquals(expectedBirthDate, result.getBirthDate());
     }
 }
