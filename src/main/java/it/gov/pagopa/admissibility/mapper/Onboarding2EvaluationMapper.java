45a7a2120c86e06aa88acad35fbfeddb129228f6
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-11-28 18:32:52 +0100

    removed unused import

--- src/main/java/it/gov/pagopa/admissibility/mapper/Onboarding2EvaluationMapper.java
+++ src/main/java/it/gov/pagopa/admissibility/mapper/Onboarding2EvaluationMapper.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
