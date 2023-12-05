7d700d79849dcb02cf906cab0f19cc1f1a43d260
frtrinca <francesco.trinca@nttdata.com>
2023-12-05 14:23:35 +0100

    Sonar fix - remove import

--- src/main/java/it/gov/pagopa/common/config/HealthIndicatorLogger.java
+++ src/main/java/it/gov/pagopa/common/config/HealthIndicatorLogger.java
@@ -1,7 +1,6 @@
 package it.gov.pagopa.common.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
