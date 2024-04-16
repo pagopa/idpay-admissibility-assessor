67dbe3964a39a2f89766bcbf59e2f66d623e9da3
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@nttdata.com>
2024-04-16 15:28:39 +0200

    [IDP-2498] added test for budget exhausted check

--- src/test/java/it/gov/pagopa/admissibility/service/InitiativeStatusServiceImplTest.java
+++ src/test/java/it/gov/pagopa/admissibility/service/InitiativeStatusServiceImplTest.java
@@ -21,12 +21,7 @@ class InitiativeStatusServiceImplTest {
         InitiativeCountersRepository initiativeCountersRepositoryMock = Mockito.mock(InitiativeCountersRepository.class);
         OnboardingContextHolderService onboardingContextHolderServiceMock = Mockito.mock(OnboardingContextHolderService.class);
 
-        InitiativeConfig initiativeConfigMock = InitiativeConfig.builder()
-                .initiativeId("INITIATIVE1")
-                .initiativeBudgetCents(1_00L)
-                .beneficiaryInitiativeBudgetCents(10_00L)
-                .status("STATUS1")
-                .build();
+        InitiativeConfig initiativeConfigMock = getInitiativeConfigForContextMock();
         Mockito.when(onboardingContextHolderServiceMock.getInitiativeConfig(Mockito.anyString())).thenReturn(Mono.just(initiativeConfigMock));
 
         InitiativeCounters initiativeCountersMock = InitiativeCounters.builder()
@@ -55,12 +50,7 @@ class InitiativeStatusServiceImplTest {
         InitiativeCountersRepository initiativeCountersRepositoryMock = Mockito.mock(InitiativeCountersRepository.class);
         OnboardingContextHolderService onboardingContextHolderServiceMock = Mockito.mock(OnboardingContextHolderService.class);
 
-        InitiativeConfig initiativeConfigMock = InitiativeConfig.builder()
-                .initiativeId("INITIATIVE1")
-                .initiativeBudgetCents(1_00L)
-                .beneficiaryInitiativeBudgetCents(10_00L)
-                .status("STATUS1")
-                .build();
+        InitiativeConfig initiativeConfigMock = getInitiativeConfigForContextMock();
         Mockito.when(onboardingContextHolderServiceMock.getInitiativeConfig(Mockito.anyString())).thenReturn(Mono.just(initiativeConfigMock));
 
         Mockito.when(initiativeCountersRepositoryMock.findById(Mockito.anyString())).thenReturn(Mono.empty());
@@ -72,5 +62,44 @@ class InitiativeStatusServiceImplTest {
 
         // Then
         Assertions.assertNull(result);
+    }
+
+    @Test
+    void testNotBudgetAvailable(){
+        // Given
+        InitiativeCountersRepository initiativeCountersRepositoryMock = Mockito.mock(InitiativeCountersRepository.class);
+        OnboardingContextHolderService onboardingContextHolderServiceMock = Mockito.mock(OnboardingContextHolderService.class);
+
+        InitiativeConfig initiativeConfigMock = getInitiativeConfigForContextMock();
+        Mockito.when(onboardingContextHolderServiceMock.getInitiativeConfig(Mockito.anyString())).thenReturn(Mono.just(initiativeConfigMock));
+
+        InitiativeCounters initiativeCountersMock = InitiativeCounters.builder()
+                .id("INITIATIVE1")
+                .initiativeBudgetCents(100L)
+                .onboarded(1L)
+                .residualInitiativeBudgetCents(100L)
+                .reservedInitiativeBudgetCents(1000L)
+                .build();
+        Mockito.when(initiativeCountersRepositoryMock.findById(Mockito.anyString())).thenReturn(Mono.just(initiativeCountersMock));
+
+        InitiativeStatusService initiativeStatusService = new InitiativeStatusServiceImpl(onboardingContextHolderServiceMock, initiativeCountersRepositoryMock);
+
+        InitiativeStatusDTO expected = new InitiativeStatusDTO("STATUS1", false);
+
+        // When
+        InitiativeStatusDTO result = initiativeStatusService.getInitiativeStatusAndBudgetAvailable("INITIATIVE1").block();
+
+        // Then
+        Assertions.assertEquals(expected, result);
+
+    }
+
+    private static InitiativeConfig getInitiativeConfigForContextMock() {
+        return InitiativeConfig.builder()
+                .initiativeId("INITIATIVE1")
+                .initiativeBudgetCents(1_00L)
+                .beneficiaryInitiativeBudgetCents(10_00L)
+                .status("STATUS1")
+                .build();
     }
 }
