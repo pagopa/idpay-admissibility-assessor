f71493817df7b9942cbbe4d85c7b2bea76ed5ae3
stefanodel <stefano11.delia@nttdata.com>
2025-01-08 14:49:27 +0100

    fix: disable mock from FamilyDataRetrieverServiceImpl

--- src/main/java/it/gov/pagopa/admissibility/service/onboarding/pdnd/FamilyDataRetrieverServiceImpl.java
+++ src/main/java/it/gov/pagopa/admissibility/service/onboarding/pdnd/FamilyDataRetrieverServiceImpl.java
@@ -1,30 +1,50 @@
 package it.gov.pagopa.admissibility.service.onboarding.pdnd;
 
-import it.gov.pagopa.admissibility.connector.rest.mock.FamilyMockRestClient;
+import it.gov.pagopa.admissibility.connector.rest.anpr.service.AnprC001RestClient;
 import it.gov.pagopa.admissibility.dto.onboarding.OnboardingDTO;
 import it.gov.pagopa.admissibility.dto.onboarding.extra.Family;
+import it.gov.pagopa.admissibility.model.PdndInitiativeConfig;
+import it.gov.pagopa.common.reactive.pdv.service.UserFiscalCodeService;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class FamilyDataRetrieverServiceImpl implements FamilyDataRetrieverService {
-    private final FamilyMockRestClient familyMockRestClient;
+    private final AnprC001RestClient anprC001RestClient;
+    private final PdndInitiativeConfig pdndInitiativeConfig;
 
+    private final UserFiscalCodeService userFiscalCodeService;
 
-    public FamilyDataRetrieverServiceImpl(FamilyMockRestClient familyMockRestClient) {
-        this.familyMockRestClient = familyMockRestClient;
+    public FamilyDataRetrieverServiceImpl(AnprC001RestClient anprC001RestClient, PdndInitiativeConfig pdndInitiativeConfig, UserFiscalCodeService userFiscalCodeService) {
+        this.anprC001RestClient = anprC001RestClient;
+        this.pdndInitiativeConfig = pdndInitiativeConfig;
+        this.userFiscalCodeService = userFiscalCodeService;
     }
 
     @Override
     public Mono<Optional<Family>> retrieveFamily(OnboardingDTO onboardingRequest, Message<String> message) {
         // TODO call PDND and re-scheduling if dailyLimit occurred
 
-        //TODO this is a mocked behavior! Replace with the real integration
-        return familyMockRestClient.retrieveFamily(onboardingRequest.getUserId())
-                .map(Optional::of);
+
+        return userFiscalCodeService.getUserFiscalCode(onboardingRequest.getUserId())
+                .flatMap(fiscalCode -> anprC001RestClient.invoke(fiscalCode, pdndInitiativeConfig))
+                .map(response -> {
+                    Family family = new Family();
+                    family.setFamilyId(response.getIdOperazioneANPR());
+                    Set<String> memberIds = response.getListaSoggetti().getDatiSoggetto()
+                            .stream()
+                            .map(datiSoggetto -> datiSoggetto.getIdentificativi().getIdANPR())
+                            .collect(Collectors.toSet());
+
+                    family.setMemberIds(memberIds);
+
+                    return Optional.of(family);
+                });
 
     }
 }
