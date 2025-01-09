c124cb58347ef6ba870b5c27679e3d2bb5b846b0
stefanodel <stefano11.delia@nttdata.com>
2025-01-09 09:30:55 +0100

    Revert "fix: test endpoint"
    This reverts commit d5679b803d6fa03e1b739badf09b45a0b210674d.

--- src/main/java/it/gov/pagopa/common/reactive/pdnd/service/BaseRestPdndServiceClient.java
+++ src/main/java/it/gov/pagopa/common/reactive/pdnd/service/BaseRestPdndServiceClient.java
@@ -71,64 +71,35 @@ public abstract class BaseRestPdndServiceClient<T, R> extends BasePdndService<R>
     }
 
     private Mono<R> invokePdndRestService(PdndAuthData pdndAuthData, Consumer<HttpHeaders> httpHeadersConsumer, String bodyString, String digest, String agidJwtSignature) {
-        // Costruisci la cURL per il logging in modo reattivo
-        return Mono.defer(() -> {
-            // Costruisci la cURL
-            StringBuilder curlCommand = new StringBuilder();
-            curlCommand.append("curl -X ").append(pdndServiceConfig.getHttpMethod())
-                    .append(" '").append(pdndServiceConfig.getPath()).append("'");
+        return PerformanceLogger.logTimingOnNext(
+                "PDND_SERVICE_INVOKE",
+                webClient.method(pdndServiceConfig.getHttpMethod())
+                        .uri(pdndServiceConfig.getPath())
+                        .headers(httpHeaders -> {
+                            httpHeadersConsumer.accept(httpHeaders);
+                            httpHeaders.setBearerAuth(pdndAuthData.getAccessToken());
+                            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                            httpHeaders.add(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name());
+                            httpHeaders.add("Digest", digest);
+                            httpHeaders.add("Agid-JWT-TrackingEvidence", pdndAuthData.getAgidJwtTrackingEvidence());
+                            httpHeaders.add("Agid-JWT-Signature", agidJwtSignature);
+                        })
+                        .bodyValue(bodyString)
+                        .retrieve()
+                        .bodyToMono(pdndServiceConfig.getResponseBodyClass())
 
-            // Aggiungi gli headers di base alla cURL
-            curlCommand.append(" -H 'Content-Type: ").append(MediaType.APPLICATION_JSON).append("'")
-                    .append(" -H 'Content-Encoding: ").append(StandardCharsets.UTF_8.name()).append("'")
-                    .append(" -H 'Digest: ").append(digest).append("'")
-                    .append(" -H 'Agid-JWT-TrackingEvidence: ").append(pdndAuthData.getAgidJwtTrackingEvidence()).append("'")
-                    .append(" -H 'Agid-JWT-Signature: ").append(agidJwtSignature).append("'")
-                    .append(" -H 'Authorization: Bearer ").append(pdndAuthData.getAccessToken()).append("'");
-
-            // Crea un oggetto HttpHeaders e lascia che httpHeadersConsumer lo modifichi
-            HttpHeaders headers = new HttpHeaders();
-            httpHeadersConsumer.accept(headers);  // Modifica gli headers con il consumer
-
-            // Aggiungi gli headers aggiuntivi alla cURL
-            headers.forEach((key, value) ->
-                    curlCommand.append(" -H '").append(key).append(": ").append(value.get(0)).append("'")
-            );
-
-            // Aggiungi il corpo della richiesta se esiste
-            if (bodyString != null && !bodyString.isEmpty()) {
-                curlCommand.append(" -d '").append(bodyString.replace("'", "\\'")).append("'");
-            }
-
-            // Log della cURL completa
-            log.info("[PDND_SERVICE_INVOKE] cURL Command: {}", curlCommand.toString());
-
-            // Esegui la richiesta reattiva
-            return webClient.method(pdndServiceConfig.getHttpMethod())
-                    .uri(pdndServiceConfig.getPath())
-                    .headers(httpHeaders -> {
-                        httpHeadersConsumer.accept(httpHeaders);
-                        httpHeaders.setBearerAuth(pdndAuthData.getAccessToken());
-                        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-                        httpHeaders.add(HttpHeaders.CONTENT_ENCODING, StandardCharsets.UTF_8.name());
-                        httpHeaders.add("Digest", digest);
-                        httpHeaders.add("Agid-JWT-TrackingEvidence", pdndAuthData.getAgidJwtTrackingEvidence());
-                        httpHeaders.add("Agid-JWT-Signature", agidJwtSignature);
-                    })
-                    .bodyValue(bodyString)
-                    .retrieve()
-                    .bodyToMono(pdndServiceConfig.getResponseBodyClass())
-                    .onErrorResume(e -> {
-                        if(e instanceof WebClientResponseException.NotFound notFoundException){
-                            log.error("[PDND_SERVICE_INVOKE] Cannot found data when invoking PDND service {}: {}", pdndServiceConfig.getAudience(), notFoundException.getResponseBodyAsString());
-                            return Mono.just(pdndServiceConfig.getEmptyResponseBody());
-                        } else if(pdndServiceConfig.getTooManyRequestPredicate().test(e)){
-                            return Mono.error(new PdndServiceTooManyRequestException(pdndServiceConfig, e));
-                        }
-                        else {
-                            return Mono.error(new IllegalStateException("[PDND_SERVICE_INVOKE] Something went wrong when invoking PDND service %s: %s".formatted(pdndServiceConfig.getAudience(), e.getMessage()), e));
-                        }
-                    });
-        });
+                        .onErrorResume(e -> {
+                            if(e instanceof WebClientResponseException.NotFound notFoundException){
+                                log.error("[PDND_SERVICE_INVOKE] Cannot found data when invoking PDND service {}: {}", pdndServiceConfig.getAudience(), notFoundException.getResponseBodyAsString());
+                                return Mono.just(pdndServiceConfig.getEmptyResponseBody());
+                            } else if(pdndServiceConfig.getTooManyRequestPredicate().test(e)){
+                                return Mono.error(new PdndServiceTooManyRequestException(pdndServiceConfig, e));
+                            }
+                            else {
+                                return Mono.error(new IllegalStateException("[PDND_SERVICE_INVOKE] Something went wrong when invoking PDND service %s: %s".formatted(pdndServiceConfig.getAudience(), e.getMessage()), e));
+                            }
+                        }),
+                x -> "[" + getClass().getSimpleName() + "] "
+        );
     }
 }
