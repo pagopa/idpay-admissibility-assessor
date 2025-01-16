092f22803cbc37d26c58b8b11dfde1a3171237c1
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-01-16 17:14:36 +0100

    pdnd config map imp and fix

--- src/main/java/it/gov/pagopa/admissibility/service/onboarding/pdnd/FamilyDataRetrieverServiceImpl.java
+++ src/main/java/it/gov/pagopa/admissibility/service/onboarding/pdnd/FamilyDataRetrieverServiceImpl.java
@@ -1,9 +1,9 @@
 package it.gov.pagopa.admissibility.service.onboarding.pdnd;
 
+import it.gov.pagopa.admissibility.config.PagoPaAnprPdndConfig;
 import it.gov.pagopa.admissibility.connector.rest.anpr.service.AnprC021RestClient;
 import it.gov.pagopa.admissibility.dto.onboarding.OnboardingDTO;
 import it.gov.pagopa.admissibility.dto.onboarding.extra.Family;
-import it.gov.pagopa.admissibility.model.PdndInitiativeConfig;
 import it.gov.pagopa.common.reactive.pdv.service.UserFiscalCodeService;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
@@ -17,11 +17,10 @@ import java.util.stream.Collectors;
 @Service
 public class FamilyDataRetrieverServiceImpl implements FamilyDataRetrieverService {
     private final AnprC021RestClient anprC021RestClient;
-    private final PdndInitiativeConfig pdndInitiativeConfig;
-
+    private final PagoPaAnprPdndConfig pdndInitiativeConfig;
     private final UserFiscalCodeService userFiscalCodeService;
 
-    public FamilyDataRetrieverServiceImpl(AnprC021RestClient anprC021RestClient, PdndInitiativeConfig pdndInitiativeConfig, UserFiscalCodeService userFiscalCodeService) {
+    public FamilyDataRetrieverServiceImpl(AnprC021RestClient anprC021RestClient, PagoPaAnprPdndConfig pdndInitiativeConfig, UserFiscalCodeService userFiscalCodeService) {
         this.anprC021RestClient = anprC021RestClient;
         this.pdndInitiativeConfig = pdndInitiativeConfig;
         this.userFiscalCodeService = userFiscalCodeService;
@@ -32,7 +31,7 @@ public class FamilyDataRetrieverServiceImpl implements FamilyDataRetrieverServic
         // TODO call PDND and re-scheduling if dailyLimit occurred
 
         return userFiscalCodeService.getUserFiscalCode(onboardingRequest.getUserId())
-                .flatMap(fiscalCode -> anprC021RestClient.invoke(fiscalCode, pdndInitiativeConfig))
+                .flatMap(fiscalCode -> anprC021RestClient.invoke(fiscalCode, pdndInitiativeConfig.getDetails().get("c021")))
                 .publishOn(Schedulers.boundedElastic())
                 .flatMap(response ->
                     Flux.fromIterable(response.getListaSoggetti().getDatiSoggetto())
