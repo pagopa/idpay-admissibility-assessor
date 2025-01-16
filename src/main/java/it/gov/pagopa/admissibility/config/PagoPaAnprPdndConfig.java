88969e9b91b425fb055d2fbdefe4241a8719a9b8
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-01-16 18:07:27 +0100

    pdnd config map imp and fix

--- src/main/java/it/gov/pagopa/admissibility/config/PagoPaAnprPdndConfig.java
+++ src/main/java/it/gov/pagopa/admissibility/config/PagoPaAnprPdndConfig.java
@@ -2,11 +2,13 @@ package it.gov.pagopa.admissibility.config;
 
 import it.gov.pagopa.admissibility.model.PdndInitiativeConfig;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 @Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "app.anpr")
 public class PagoPaAnprPdndConfig {
