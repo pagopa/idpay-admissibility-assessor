17ad9ed1f0b28804aa55134f44c94b459406d496
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@emeal.nttdata.com>
2025-09-11 11:39:31 +0200

    [UPBE-145] Added test cases

--- src/main/java/it/gov/pagopa/admissibility/connector/soap/inps/service/IseeThresholdConsultationSoapClientImpl.java
+++ src/main/java/it/gov/pagopa/admissibility/connector/soap/inps/service/IseeThresholdConsultationSoapClientImpl.java
@@ -33,8 +33,8 @@ public class IseeThresholdConsultationSoapClientImpl implements IseeThresholdCon
     private ConsultazioneSogliaIndicatoreRequestType getRequest(String fiscalCode, String thresholdCode) {
         ConsultazioneSogliaIndicatoreRequestType consultazioneSogliaIndicatoreRequestType = new ConsultazioneSogliaIndicatoreRequestType();
 
-        consultazioneSogliaIndicatoreRequestType.setCodiceSoglia(thresholdCode);
         consultazioneSogliaIndicatoreRequestType.setCodiceFiscale(fiscalCode);
+        consultazioneSogliaIndicatoreRequestType.setCodiceSoglia(thresholdCode);
         consultazioneSogliaIndicatoreRequestType.setFornituraNucleo(SiNoEnum.SI);
         consultazioneSogliaIndicatoreRequestType.setDataValidita(DatatypeFactory.newDefaultInstance().newXMLGregorianCalendar(new GregorianCalendar()));  //TODO TBV field not required
 
