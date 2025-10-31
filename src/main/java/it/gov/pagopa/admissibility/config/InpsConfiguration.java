81b7e13d6043fb9c29bba475b2ab7613a7fa7e0c
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@emeal.nttdata.com>
2025-10-31 10:05:56 +0100

    [UPBE-223] test threshold Controller

--- src/main/java/it/gov/pagopa/admissibility/config/InpsConfiguration.java
+++ src/main/java/it/gov/pagopa/admissibility/config/InpsConfiguration.java
@@ -2,12 +2,14 @@ package it.gov.pagopa.admissibility.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+@Slf4j
 @Getter
 @Setter
 @Configuration
@@ -55,6 +57,7 @@ public class InpsConfiguration {
                     byte[] decodedBytes = Base64.getDecoder().decode(cert);
                     this.cert = new String(decodedBytes, StandardCharsets.UTF_8);
                 } catch (IllegalArgumentException e) {
+                    log.error("[INPS_CONFIG] Cannot decode cert");
                     this.cert = null;
                 }
             } else {
