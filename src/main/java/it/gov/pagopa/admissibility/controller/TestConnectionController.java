63c8c87e2dc7f3ee9076fd4682ccd3099b231b16
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@emeal.nttdata.com>
2025-10-31 11:33:56 +0100

    [UPBE-223] test threshold Controller

--- src/main/java/it/gov/pagopa/admissibility/controller/TestConnectionController.java
+++ src/main/java/it/gov/pagopa/admissibility/controller/TestConnectionController.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.admissibility.controller;
 
 import it.gov.pagopa.admissibility.dto.onboarding.OnboardingRejectionReason;
+import it.gov.pagopa.admissibility.generated.soap.ws.client.soglia.ConsultazioneSogliaIndicatoreResponse;
+import it.gov.pagopa.admissibility.generated.soap.ws.client.soglia.ConsultazioneSogliaIndicatoreResponseType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -14,4 +16,7 @@ import java.util.Optional;
 public interface TestConnectionController {
     @GetMapping(value = "/inps/connection/test/threshold/{threshold}")
     Mono<Optional<List<OnboardingRejectionReason>>> getThreshold(@PathVariable("threshold") String threshold, @RequestHeader("X-User-Code") String userCode);
+
+    @GetMapping(value = "/inps/connection/test/threshold/{threshold}/v2")
+    Mono<ConsultazioneSogliaIndicatoreResponse> getThresholdV2(@PathVariable("threshold") String threshold, @RequestHeader("X-User-Code") String userCode);
 }
--- src/main/java/it/gov/pagopa/admissibility/controller/TestConnectionControllerImpl.java
+++ src/main/java/it/gov/pagopa/admissibility/controller/TestConnectionControllerImpl.java
@@ -2,33 +2,60 @@ package it.gov.pagopa.admissibility.controller;
 
 import it.gov.pagopa.admissibility.config.PagoPaAnprPdndConfig;
 import it.gov.pagopa.admissibility.connector.pdnd.PdndServicesInvocation;
+import it.gov.pagopa.admissibility.connector.soap.inps.config.InpsThresholdClientConfig;
 import it.gov.pagopa.admissibility.connector.soap.inps.service.InpsThresholdRetrieverService;
-import it.gov.pagopa.admissibility.dto.onboarding.InitiativeStatusDTO;
 import it.gov.pagopa.admissibility.dto.onboarding.OnboardingDTO;
 import it.gov.pagopa.admissibility.dto.onboarding.OnboardingRejectionReason;
+import it.gov.pagopa.admissibility.generated.soap.ws.client.soglia.*;
+import it.gov.pagopa.common.reactive.soap.utils.SoapUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import javax.xml.datatype.DatatypeFactory;
 import java.util.Collections;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Optional;
+
+import static it.gov.pagopa.common.reactive.web.ReactiveRequestContextHolder.getRequest;
 
 @RestController
 @Slf4j
 public class TestConnectionControllerImpl implements TestConnectionController{
+
+    private static final DatatypeFactory DATATYPE_FACTORY = DatatypeFactory.newDefaultInstance();
+
     private final InpsThresholdRetrieverService inpsThresholdRetrieverService;
     private final PagoPaAnprPdndConfig pagoPaAnprPdndConfig;
 
-    public TestConnectionControllerImpl(InpsThresholdRetrieverService inpsThresholdRetrieverService, PagoPaAnprPdndConfig pagoPaAnprPdndConfig) {
+    private final ISvcConsultazione portSvcConsultazione;
+
+    public TestConnectionControllerImpl(InpsThresholdRetrieverService inpsThresholdRetrieverService, PagoPaAnprPdndConfig pagoPaAnprPdndConfig, InpsThresholdClientConfig inpsThresholdClientConfig) {
         this.inpsThresholdRetrieverService = inpsThresholdRetrieverService;
         this.pagoPaAnprPdndConfig = pagoPaAnprPdndConfig;
+        this.portSvcConsultazione = inpsThresholdClientConfig.getPortSvcConsultazione();
     }
 
     @Override
     public Mono<Optional<List<OnboardingRejectionReason>>> getThreshold(String threshold, String userCode) {
         PdndServicesInvocation pdndServicesInvocation = new PdndServicesInvocation(false, Collections.emptyList(), false, false, true, threshold);
         return inpsThresholdRetrieverService.invoke(userCode,pagoPaAnprPdndConfig.getPagopaPdndConfiguration().get("c001"), pdndServicesInvocation,  new OnboardingDTO());
+
+    }
+
+    @Override
+    public Mono<ConsultazioneSogliaIndicatoreResponse> getThresholdV2(String threshold, String userCode) {
+
+        ConsultazioneSogliaIndicatoreRequestType consultazioneSogliaIndicatoreRequestType = new ConsultazioneSogliaIndicatoreRequestType();
+
+        consultazioneSogliaIndicatoreRequestType.setCodiceFiscale(userCode);
+        consultazioneSogliaIndicatoreRequestType.setCodiceSoglia(threshold);
+        consultazioneSogliaIndicatoreRequestType.setFornituraNucleo(SiNoEnum.SI);
+        consultazioneSogliaIndicatoreRequestType.setDataValidita(DATATYPE_FACTORY.newXMLGregorianCalendar(new GregorianCalendar()));
+
+        return SoapUtils.soapInvoke2Mono(asyncHandler ->
+                portSvcConsultazione.consultazioneSogliaIndicatoreAsync(consultazioneSogliaIndicatoreRequestType, asyncHandler));
 
     }
 }
