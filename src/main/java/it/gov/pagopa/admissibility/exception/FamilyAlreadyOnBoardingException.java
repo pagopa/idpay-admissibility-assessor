bfdabacd7d9e5ccdb3249aa2123ed2f41bf3c2da
GiovanaSolorzano <105281122+GiovanaSolorzano@users.noreply.github.com>
2025-11-06 10:11:21 +0100

    fix: Upbe 336 be controllo nucleo familiare error handler (#356)

---
+++ src/main/java/it/gov/pagopa/admissibility/exception/FamilyAlreadyOnBoardingException.java
@@ -0,0 +1,5 @@
+package it.gov.pagopa.admissibility.exception;
+
+/** Exception thrown in order check another families member */
+public class FamilyAlreadyOnBoardingException extends RuntimeException {
+}
