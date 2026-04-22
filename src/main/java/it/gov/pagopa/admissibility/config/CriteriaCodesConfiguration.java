41484dba682c50bcb2c972775128be616985d6ff
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2026-04-22 11:25:17 +0200

    enable configuration

--- src/main/java/it/gov/pagopa/admissibility/config/CriteriaCodesConfiguration.java
+++ src/main/java/it/gov/pagopa/admissibility/config/CriteriaCodesConfiguration.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+
 
 @ConfigurationProperties(prefix = "app.criteria-code")
 @Data
