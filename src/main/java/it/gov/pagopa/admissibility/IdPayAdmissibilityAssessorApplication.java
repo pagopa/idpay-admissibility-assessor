4251237609efcb2062efb41090a1c38c68a28ef9
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2026-04-22 09:35:59 +0200

    enable configuration

--- src/main/java/it/gov/pagopa/admissibility/IdPayAdmissibilityAssessorApplication.java
+++ src/main/java/it/gov/pagopa/admissibility/IdPayAdmissibilityAssessorApplication.java
@@ -1,10 +1,13 @@
 package it.gov.pagopa.admissibility;
 
 
+import it.gov.pagopa.admissibility.config.CriteriaCodeConfigs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(scanBasePackages = "it.gov.pagopa")
+@EnableConfigurationProperties(CriteriaCodeConfigs.class)
 public class IdPayAdmissibilityAssessorApplication {
 
     public static void main(String[] args) {
